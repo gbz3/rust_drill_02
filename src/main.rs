2da7bbb0b5e28c464cf77a689ab9ef0d96506d55
gbz3 <1808483+gbz3@users.noreply.github.com>
2024-07-28 10:33:35 +0900

    use clap crate.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+
+    #[arg(short = 'n', help = "Do not print newline")]
+    omit_newline: bool,
+
+    #[arg(value_name = "TEXT", required = true, help = "Input text")]
+    inputs: Vec<String>,
+
+}
+
 fn main() {
-    println!("{:?}", std::env::args());
+    let args = Args::parse();
+
+    println!("{:#?}", args);
 }
